d7989b6b2e1d46640bbe4ec0eb1f4507ef762c7e
Ian Kosen <ikosenn@gmail.com>
2015-05-15 07:16:59 +0300

    Resolved the errors

--- myCal.js
+++ myCal.js
@@ -1,7 +1,7 @@
 
-var myCal =angular.module('myCal',['service']);
+var myCal = angular.module('myCal',[]);
 
-myCal.controller('myCalController',['$scope','notifications',function($scope,notifications){
+myCal.controller('myCalController', ['$scope','notifications',function($scope, notifications){
 	$scope.total = 0;
 	$scope.temp=0;
 	this.add=function(){
@@ -14,31 +14,48 @@ myCal.controller('myCalController',['$scope','notifications',function($scope,not
 		$scope.temp=0;
 	};
 	this.mul=function () {
+
+		if($scope.temp >= 1 && $scope.total == 0){
+			$scope.total = 1;
+		}
 		$scope.total=$scope.total * $scope.temp;
 		$scope.temp=0;
 	};
 	this.div=function () {
+		if($scope.temp >= 1 && $scope.total == 0){
+			$scope.total = 1;
+		}
 		$scope.total=$scope.total / $scope.temp;
 		$scope.temp=0;
 	};
 	this.setTemp=function(number){
-		$scope.temp = this.temp.toString() + number.toString();
+
+		$scope.temp = $scope.temp.toString() + number.toString();
 		$scope.temp = parseInt($scope.temp,10);
 	};
 	this.result=function(){
+		$scope.total = $scope.total + $scope.temp;
 		notifications($scope.total);
-
-	}
+	};
 }]);
 
 myCal.directive('loadHeader', function(){
 	
 	return{
-		restrict: 'E',
+		restrict: 'A',
 		templateUrl: 'awesome.html'
 
 	};
 });
 
+
+
+myCal.factory('notifications', ['$window', function(win, answer){
+
+	string = "The operation resulted to: " + answer;
+
+	return function(string){win.alert(string)};
+
+}]);
 
 
