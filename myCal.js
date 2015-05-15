819a83fe97bd20bd7954d55133c03fa20ce46384
Ian Kosen <ikosenn@gmail.com>
2015-05-15 08:04:45 +0300

    Error edits

--- myCal.js
+++ myCal.js
@@ -7,26 +7,30 @@ myCal.controller('myCalController', ['$scope','notifications',function($scope, n
 	this.add=function(){
 		$scope.total = $scope.total + $scope.temp;
 		$scope.temp = 0;
+		this.previous_op = 0;
 	};
 	this.sub=function () {
 
 		$scope.total=$scope.total - $scope.temp;
 		$scope.temp=0;
+		this.previous_op = 1;
 	};
 	this.mul=function () {
 
 		if($scope.temp >= 1 && $scope.total == 0){
 			$scope.total = 1;
 		}
-		$scope.total=$scope.total * $scope.temp;
+		$scope.total=$scope.temp * $scope.total;
 		$scope.temp=0;
+		this.previous_op = 2;
 	};
 	this.div=function () {
 		if($scope.temp >= 1 && $scope.total == 0){
 			$scope.total = 1;
 		}
-		$scope.total=$scope.total / $scope.temp;
+		$scope.total= $scope.temp / $scope.total;
 		$scope.temp=0;
+		this.previous_op = 3;
 	};
 	this.setTemp=function(number){
 
@@ -34,7 +38,18 @@ myCal.controller('myCalController', ['$scope','notifications',function($scope, n
 		$scope.temp = parseInt($scope.temp,10);
 	};
 	this.result=function(){
-		$scope.total = $scope.total + $scope.temp;
+		if (this.previous_op == 1){
+			$scope.total = $scope.total - $scope.temp;
+		}
+		else if (this.previous_op == 0){
+			$scope.total = $scope.total + $scope.temp;
+		}
+		else if (this.previous_op == 2){
+			$scope.total = $scope.total * $scope.temp;
+		}
+		else{
+			$scope.total = $scope.total / $scope.temp;
+		}
 		notifications($scope.total);
 	};
 }]);
